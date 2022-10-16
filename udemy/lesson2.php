d1a029febf104f5426a07dcb41ffbad2e2fd534b
hayatek <stream.hyt@gmail.com>
2022-10-16 09:39:50 +0900

    addded lesson2.php

--- udemy/lesson2.php
+++ udemy/lesson2.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="jp">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DB接続</title>
 </head>
 
 <body>
     <?php
-    echo 'Hello, I\'m studying'
+    $db = new mysqli('localhost:8889', 'root', 'root', 'myDB');
+    echo "DBに接続しました。"
     ?>
+
 </body>
 
 </html>
