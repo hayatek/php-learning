fa20334dee1aebd2bd899f85dcebb13a8f5cde41
hayatek <stream.hyt@gmail.com>
2022-10-30 09:26:39 +0900

    added renso_array.php

---
+++ udemy/renso_array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB接続</title>
+</head>
+
+<body>
+    <?php
+    //連想配列
+    $fruits = [
+        'apple' => 'リンゴ',
+        'grape' => 'ぶどう',
+        'lemon' => 'レモン',
+        'tomato' => 'トマト',
+        'peach' => 'ピーチ'
+    ];
+    echo $fruits['tomato'];
+    ?>
+
+</body>
+
+</html>
